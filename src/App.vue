34737f1d488450fc2af691b06e9f7f635e5bb141
Tony-tuf15 <a.gryhoryev@gmail.com>
2023-01-20 14:35:08 +0200

    test

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <TodoList :tasks="todos" @del-task="deleteTask"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TaskList.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      todos:['1','2','check']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods:{
+    deleteTask(i){
+      this.todos.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(todo, index) in tasks"
+        @click="deleteTask"
+        :key="index"
+      >
+        {{ index + 1 }}: {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tasks"],
+  methods: {
+    deleteTask(index) {
+      this.$emit("del-task", index);
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+li {
+  list-style: none;
+  font-size: 30px;
+  border: 1px dotted grey;
+  width: 50%;
+  cursor: pointer;
+}
+</style>
